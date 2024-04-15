4117366e1b60c1b7db57bf406dccd61b9913e736
Samael <jeremie.joannard@epitech.eu>
2024-04-16 05:00:13 +0900

    [ADD] ./,
    Adding file listing basics

---
+++ rust_ls/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+fn get_files(filename: String) -> Result<Vec<String>, std::io::Error> {
+    let mut files = Vec::new();
+    if let Ok(entries) = fs::read_dir(filename) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Some(file_name) = entry.file_name().to_str() {
+                    files.push(file_name.to_string());
+                }
+            }
+        }
+    }
+    Ok(files)
+}
+
+fn basic_print_files(file_names: Vec<String>) {
+    for file in file_names {
+        //if file.starts_with(".") {
+        //    continue;
+        //}
+        if file.starts_with(".") {
+            continue;
+        }
+        println!("{}", file);
+    }
+}
+
+fn main() {
+    let files = get_files(".".to_string()).unwrap();
+    basic_print_files(files);
+}
